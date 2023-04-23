8c530bb1ebb427b6f72c2bc698a4276b20595b4a
Ramazan ATALAY <ramazannatalayy@gmail.com>
2023-04-24 00:13:16 +0300

    Harmonic Mean Calculations

---
+++ Harmonik_Ortalama/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double [] numbers = {1,2,3,4,5,6};
+
+        double harmonik = 0;
+
+        for (int i=0 ;i<numbers.length;i++) {
+
+            harmonik +=  (1/numbers[i]);
+
+        }
+        System.out.println("Dizinin harmonik ortalaması: "+(harmonik));
+
+    }
+}
