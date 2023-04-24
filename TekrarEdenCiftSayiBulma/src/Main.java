c2140d24b42b691ac2bde3b132951b9ac0cb77f8
Ramazan ATALAY <ramazannatalayy@gmail.com>
2023-04-25 02:44:54 +0300

    To find the repeated numbers we used the arrays.

---
+++ TekrarEdenCiftSayiBulma/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    static boolean find(int [] array, int value){
+
+        for (int i:array){
+            if (i==value){return true;}
+
+        }
+            return false;
+    }
+
+    public static void main(String[] args) {
+
+        int [] list = {8,1,2,3,4,3,5,6,7,7,8,9,10,10,11,7,12,13,14,14};
+        int [] repeat = new int[list.length];
+        int start=0;
+                for(int i=0;i< list.length;i++){
+
+
+                    for (int j=0;j<list.length;j++){
+
+                        if((i != j) && list[i] == list[j]) {
+
+                            if (!find(repeat,list[i])){
+                                repeat[start++] = list[i];}
+
+                            break;
+
+                        }
+
+                    }
+
+                }
+        System.out.println(Arrays.toString(repeat));
+    }
+}
